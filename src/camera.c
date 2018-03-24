a289eb623bed4f4c308043f90bbdb275f6cb4240
Yann VILLEPONTOUX <yvillepo@e3r9p3.42.fr>
2018-03-24 14:52:52 +0100

    correction erreur gestion camera

--- src/camera.c
+++ src/camera.c
@@ -6,11 +6,27 @@
 /*   By: yvillepo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/20 12:51:26 by yvillepo          #+#    #+#             */
-/*   Updated: 2018/03/23 07:25:13 by yvillepo         ###   ########.fr       */
+/*   Updated: 2018/03/24 14:50:42 by yvillepo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
+
+static int	exeption(t_mlx *mlx, t_vect *dir, int x, int y)
+{
+	t_vect	*v;
+
+	v = mlx->camera_dir;
+	if (v->x == 0 && v->z == 0)
+	{
+		dir->x = (mlx->h / 2) - mlx->pitch * x;
+		dir->y = v->y;
+		dir->z = (mlx->h / 2) - mlx->pitch * y;
+		v_unit(dir);
+		return (1);
+	}
+	return (0);
+}
 
 void		calc_ray(t_mlx *mlx, t_vect *dir, double x, double y)
 {
@@ -19,6 +35,8 @@ void		calc_ray(t_mlx *mlx, t_vect *dir, double x, double y)
 	t_vect	*j;
 	t_vect	*k;
 
+	if (exeption(mlx, dir, x, y))
+		return ;
 	uv.x = (mlx->h / 2) - mlx->pitch * x;
 	uv.y = (mlx->h / 2) - mlx->pitch * y;
 	k = mlx->camera_dir;
